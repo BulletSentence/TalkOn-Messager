2a3f38231856962e0664c71e5b8e5d89facb6216
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-11 04:41:02 -0300

    Firebase test

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  
+
   runApp(MyApp());
+  Firestore.instance.collection("col").document("doc").setData({"text": "Almeida"});
 }
 
 class MyApp extends StatelessWidget {
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
