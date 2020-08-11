0022b5911a70f0ba9cf76afc6b614c1251a99dd0
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-11 11:12:53 -0300

    Messager Screen

---
+++ lib/chat_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("User"),
+        elevation: 1,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-
   runApp(MyApp());
-
-  QuerySnapshot snapshot = await Firestore.instance.collection("mensagem").getDocuments();
-  snapshot.documents..forEach((d){
-    print(d.data);
-  });
 }
 
 class MyApp extends StatelessWidget {
@@ -16,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'TalkOn Messager',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
