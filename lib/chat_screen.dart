2e45e48e7abacb09621c5776a914863c9eb39bcf
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-16 13:58:57 -0300

    uid test

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -18,6 +17,7 @@ class _ChatScreenState extends State<ChatScreen> {
   final GoogleSignIn googleSignIn = GoogleSignIn();
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   FirebaseUser _currentUser;
+  bool _isLoading  = false;
 
   @override
   void initState() {
@@ -61,6 +61,7 @@ class _ChatScreenState extends State<ChatScreen> {
       'uid': user.uid,
       'senderName': user.displayName,
       'senderPhotoUrl': user.photoUrl,
+      'time' : Timestamp.now(),
     };
 
     if (imgFile != null) {
@@ -69,9 +70,18 @@ class _ChatScreenState extends State<ChatScreen> {
           .child(DateTime.now().millisecondsSinceEpoch.toString())
           .putFile(imgFile);
 
+      setState(() {
+        _isLoading = true;
+      });
+
       StorageTaskSnapshot takeSnapshot = await task.onComplete;
       String url = await takeSnapshot.ref.getDownloadURL();
       data['imgUrl'] = url;
+
+      setState(() {
+        _isLoading = false;
+      });
+
     }
     if (text != null) data['text'] = text;
     Firestore.instance.collection("mensagem").add(data);
@@ -105,7 +115,7 @@ class _ChatScreenState extends State<ChatScreen> {
         children: <Widget>[
           Expanded(
             child: StreamBuilder<QuerySnapshot>(
-              stream: Firestore.instance.collection('messages').snapshots(),
+              stream: Firestore.instance.collection('messages').orderBy('time').snapshots(),
               builder: (context, snapshot) {
                 switch (snapshot.connectionState) {
                   case ConnectionState.none:
@@ -120,12 +130,15 @@ class _ChatScreenState extends State<ChatScreen> {
                         itemCount: documents.length,
                         reverse: true,
                         itemBuilder: (context, index) {
-                          return ChatMessager(documents[index].data, true);
+                          return ChatMessager(
+                            documents[index].data,
+                            documents[index].data['uid'] == _currentUser?.uid);
                         });
                 }
               },
             ),
           ),
+          _isLoading ? LinearProgressIndicator() : Container(),
           TextComposer(_sendMsgToDatabase),
         ],
       ),
