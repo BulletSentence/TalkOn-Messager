7ae37e03551b7f3b3312057185557d47574728d0
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-09-28 11:53:40 -0300

    atualização

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'const.dart';
+import 'login.dart';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TalkOn Messager',
+      title: 'Chat Demo',
       theme: ThemeData(
-          primarySwatch: Colors.red,
-          iconTheme: IconThemeData(
-            color: Colors.red,
-          )
+        primaryColor: themeColor,
       ),
-      home: null,
+      home: LoginScreen(title: 'CHAT DEMO'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
